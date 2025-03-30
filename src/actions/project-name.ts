63e5f0dd70b8592388374b04e40a9fdb419b0ab5
USpiri <urielspiridione135@gmail.com>
2025-03-30 13:01:24 -0300

    ✨ Feat: Check initial value for project name

--- src/actions/project-name.ts
+++ src/actions/project-name.ts
@@ -1,14 +1,20 @@
-import { isCancel, text } from "@clack/prompts";
+import { isCancel, log, text } from "@clack/prompts";
 import { DEFAULT_PROJECT_NAME } from "../config";
 import { cancel } from "../utils";
 
 export const projectName = async (initialValue?: string) => {
-  if (initialValue) return { name: initialValue };
+  const grammarCheck = validateGrammar(initialValue);
+  if (initialValue && grammarCheck) return { name: initialValue };
+
+  const shouldShowMessage = initialValue && !grammarCheck;
+  if (shouldShowMessage) log.warn(`Invalid project name "${initialValue}"`);
 
   const namePrompt = await text({
-    message: "Project name:",
-    defaultValue: DEFAULT_PROJECT_NAME,
-    placeholder: DEFAULT_PROJECT_NAME,
+    message: !shouldShowMessage
+      ? "Project name:"
+      : "Provide a valid name for your project:",
+    defaultValue: initialValue ?? DEFAULT_PROJECT_NAME,
+    placeholder: initialValue ?? DEFAULT_PROJECT_NAME,
     validate,
   });
   if (isCancel(namePrompt)) return cancel();
@@ -17,8 +23,11 @@ export const projectName = async (initialValue?: string) => {
 };
 
 const validate = (value: string) => {
-  if (value.match(/[^a-zA-Z0-9-_]+/g))
+  if (!validateGrammar(value))
     return "Project name can only contain letters, numbers, dashes and underscores";
 
   if (value.length === 0) return "You must provide a project name";
 };
+
+const validateGrammar = (value?: string) =>
+  !!value && /^[a-zA-Z0-9-_]+$/.test(value);
